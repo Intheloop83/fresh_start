4fc75f9a69186e33a2c9a17ea7f2cd1fd5d30bb8
bngwanforbi <brendan.ngwanforbi@gmail.com>
2021-12-05 22:03:32 -0800

    Made changes to the following files: fresh_start/migrations/0001_initial.py, fresh_start/templates/fresh_start/base.html, fresh_start/templates/fresh_start/resources.html, fresh_start/urls.py, fresh_start/views.py. Created the resources page. Resources can be added from the admin page to the resources.html page.

--- mysite/fresh_start/urls.py
+++ mysite/fresh_start/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import ResourcePostListView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='fresh_start-home'),
     path('about/', views.about, name='fresh_start-about'),
+    path('resources/', ResourcePostListView.as_view(), name='fresh_start-resources'),
 ]
