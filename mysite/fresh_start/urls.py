a09e2209548e4b6f8830278987ac53d5f0d74c2e
suenodivino <depena210@gmail.com>
2021-12-07 23:24:59 -0500

    added add_resources and comments html files and updated urls, views and base,html

--- mysite/fresh_start/urls.py
+++ mysite/fresh_start/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='fresh_start-home'),
     path('about/', views.about, name='fresh_start-about'),
+    path("", include("add_resources.urls")),
+    path("comments/", include("comment.urls")),
+    path("admin/", admin.site.urls),
 ]
--- mysite/fresh_start/views.py
+++ mysite/fresh_start/views.py
@@ -7,3 +7,11 @@ def home(request):
 
 def about(request):
     return render(request, 'fresh_start/about.html', {'title': 'About'})
+
+
+def add_resources(request):
+    return render(request, 'fresh_start/add_reources.html', {'title': 'Add Resources'})
+
+
+def comments(request):
+    return render(request, 'fresh_start/comments.html', {'title': 'Comments'})
