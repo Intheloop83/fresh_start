1612d430225f5843aebccc0207bbfdbfbac9cc73
emmacor <emmacornel83@gmail.com>
2021-12-08 19:38:43 -0500

    Updates

--- mysite/fresh_start/urls.py
+++ mysite/fresh_start/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='fresh_start-home'),
     path('about/', views.about, name='fresh_start-about'),
-    path('resources/', ResourcePostListView.as_view(), name='fresh_start-resources'),
+    path('resources/', ResourcePostListView.as_view(),
+         name='fresh_start-resources'),
     path('post/<pk>/', ResourcePostDetailView.as_view(), name='post-detail'),
+    path('resourcecom/', views.about, name='fresh_start-resourcecom'),
+
+
+
 ]
