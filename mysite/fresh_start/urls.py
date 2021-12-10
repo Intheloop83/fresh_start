1a9bc4c452f19698a2d233e0680e85dd472973dc
suenodivino <depena210@gmail.com>
2021-12-10 09:36:18 -0500

    Updated files

--- mysite/fresh_start/urls.py
+++ mysite/fresh_start/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     path('', views.home, name='fresh_start-home'),
     path('about/', views.about, name='fresh_start-about'),
     path('', views.add_resources, name='add_resources.html'),
-    path('', views.comments, name='comments.html'),
-    #path('', views.index , name='index'),
-    #url(r'^$', 'index', name='index'),
 ]
--- mysite/fresh_start/views.py
+++ mysite/fresh_start/views.py
@@ -11,7 +11,3 @@ def about(request):
 
 def add_resources(request):
     return render(request, 'fresh_start/add_reources.html', {'title': 'Add Resources'})
-
-
-def comments(request):
-    return render(request, 'fresh_start/comments.html', {'title': 'Comments'})
