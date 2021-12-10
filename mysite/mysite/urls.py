ed1fbf8d594f6705bfc731ecde937ce23fb09e29
suenodivino <depena210@gmail.com>
2021-12-10 09:58:37 -0500

    Updated files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fresh_start.urls')),
-    path('', include('add_resources.html')),
 ]
