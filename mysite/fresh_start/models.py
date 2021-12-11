411c831bf42ca95f098ba8b36016fcee39bd0927
emmacor <emmacornel83@gmail.com>
2021-12-11 13:20:07 -0500

    Changes

--- mysite/fresh_start/models.py
+++ mysite/fresh_start/models.py
@@ -4,18 +4,8 @@ from datetime import datetime
 # Create your models here.
 
 
-class Categories(models.TextChoices):
-    FOOD = 'food'
-
-
 class ResourcePost(models.Model):
     title = models.CharField(max_length=50)
-    slug = models.SlugField()
-    category = models.CharField(
-        max_length=50, choices=Categories.choices, default=Categories.FOOD)
     thumbnail = models.URLField()
-    month = models.CharField(max_length=3)
-    day = models.CharField(max_length=2)
-    content = models.TextField()
     featured = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.now, blank=True)
