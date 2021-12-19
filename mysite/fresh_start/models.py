f84a6881d00a0ff7b0877a81c0b632df2f528238
suenodivino <depena210@gmail.com>
2021-12-19 08:37:45 -0500

    Added tag and comment models

--- mysite/fresh_start/models.py
+++ mysite/fresh_start/models.py
@@ -11,5 +11,14 @@ class ResourcePost(models.Model):
     featured = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=datetime.now, blank=True)
 
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    body = models.CharField(max_length=255)
+    post_id = models.ForeignKey(ResourcePost, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
     # def get_absolute_url(self):
     # return reverse('post-detail', kwargs={'pk: self.pk'})
